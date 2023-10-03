353eef0bbf868765024439a6fb46e69ede10a8bd
Daishimomura1 <140924361+Daishimomura1@users.noreply.github.com>
2023-10-03 16:47:03 -0300

    CALDEIRA

---
+++ src/exercicios/aula1/exercicio1.java
@@ -0,0 +1,8 @@
+package exercicios.aula1;
+
+class ImprimirNome {
+    public static void main(String[] args) {
+        String nome = "Dai";
+        System.out.println("Meu nome é: " + nome);
+    }
+}
