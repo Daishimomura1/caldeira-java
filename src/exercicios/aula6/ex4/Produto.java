12b89d6df433486d356092c1db674486d1640349
Daishimomura1 <dai.shimomura2003@gmail.com>
2023-11-16 10:00:54 -0300

    Caldeira

---
+++ src/exercicios/aula6/ex4/Produto.java
@@ -0,0 +1,21 @@
+package exercicios.aula6.ex4;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+        public class Produto {
+            public static void main(String[] args) {
+                LinkedHashMap<String, Double> produto = new LinkedHashMap<>();
+
+                produto.put("Chocolate", 6.45);
+                produto.put("Cerveja", 10.00);
+                produto.put("Batata", 7.98);
+                produto.put("Refrigerante", 5.00);
+                produto.put("Laranja", 4.99);
+
+
+
+                for (Map.Entry<String, Double> mapElementos : produto.entrySet()) {
+                    System.out.println(mapElementos.getKey() + "= " + mapElementos.getValue());
+                }
+            }
+        }
